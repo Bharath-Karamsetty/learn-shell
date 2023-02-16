85ac754ee6a00e2013ab5c51aa2971d9b63a1162
Bharath <bharath.necg@gmail.com>
2023-02-16 17:52:39 +0530

    Added Shell scripting topics

---
+++ 02.variable.sh
@@ -0,0 +1,8 @@
+# if we assign a name to set of data is called as variable
+
+course=Devops
+learner=Bharath
+
+# we use any of below two syntax for accessing variable in bash shell
+echo Course Name - $course
+echo Learner Name - ${learner}
