1132bba9580ce567fe2b3ae164594d980a92ca34
Bharath <bharath.necg@gmail.com>
2023-02-16 18:01:09 +0530

    Added Shell scripting topics

--- 02.variable.sh
+++ 02.variable.sh
@@ -7,5 +7,10 @@ learner=Bharath
 echo Course Name - $course
 echo Learner Name - ${learner}
 
+# For getting the data dynamically, use open brackets in syntax
 Date=$(date +%F)
 echo Today Date is $Date
+
+# For Arithmetic Operation use below syntax
+Add=${(2+3)}
+echo ADD - $Add
