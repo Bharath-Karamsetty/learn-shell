5fd1c357ec892c0ee4fc29edcabdf19a90143d29
Bharath <bharath.necg@gmail.com>
2023-02-16 17:56:20 +0530

    Added Shell scripting topics

--- 02.variable.sh
+++ 02.variable.sh
@@ -6,3 +6,6 @@ learner=Bharath
 # we use any of below two syntax for accessing variable in bash shell
 echo Course Name - $course
 echo Learner Name - ${learner}
+
+Date=$(date +%F)
+echo Today Date is - $Date
