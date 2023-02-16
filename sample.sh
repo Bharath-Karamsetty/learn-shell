9bcdf7bdda84fb1d5a2e85b1256ac2bd651bfeba
Bharath <bharath.necg@gmail.com>
2023-02-16 16:57:55 +0530

    Added Shell scripting topics

--- sample.sh
+++ sample.sh
@@ -1,4 +1,8 @@
-ls
-cat /etc/passwd | head -2
+#!/bin/bash
 
+#List of File
+ls
+#read top two lines in /etc/passwd file
+cat /etc/passwd | head -2
+#serach root user in /etc/passwd file
 grep root /etc/passwd
